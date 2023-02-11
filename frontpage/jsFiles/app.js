458d26e665844026f397d17a079a7689f871511f
arpitras <96791790+arpitras@users.noreply.github.com>
2023-02-12 05:14:08 +0530

    Add files via upload

---
+++ frontpage/jsFiles/app.js
@@ -0,0 +1,182 @@
+const express=require("Express");
+const https= require("https");
+const path=require("path");
+const bodyparser= require("body-parser");
+const { response } = require("Express");
+const app=express();
+app.use(bodyparser.urlencoded({extended:true}));
+const mongoose = require("mongoose");
+const { time } = require("console");
+mongoose.set('strictQuery', false);
+
+const mongoDB = "mongodb://127.0.0.1/TRYMNNIT";
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+const Schema = mongoose.Schema;
+
+
+const str=__dirname;
+const str2=path.join(__dirname,"../");
+//console.log(str2);
+app.use(express.static(str2)); 
+console.log(str2);
+
+const QASchema =new Schema
+({
+   ques :String ,
+   ans : String
+});
+const LoginSchema = new Schema
+({
+  emailid : String,
+  username :String,
+  password :String
+});
+const Tasksf =mongoose.model("LoginIDFarmer",LoginSchema);
+const Taskse =mongoose.model("LoginIDEnthu",LoginSchema);
+// USe Tasksf for handling farmers database
+// USe TAskse for handling enthusiasts database
+const t1=new Tasksf({
+  emailid : "arpit@gmail.com",
+  username :"arpitras",
+  password : "naman"
+});
+const t2= new Taskse(
+{
+emailid : "arsh@gmail.com",
+username :"arshweb",
+password : "naman"
+});
+
+// t1.save();
+// t2.save();
+
+const QA =mongoose.model("QADB",QASchema);
+
+const q1 =new QA({
+  ques:"Best season for planting rice?",
+  ans:"Not Answered"
+});
+q1.save();
+app.post("/fsignin.html",function(req,res)
+{
+  // const res2=req.body.s1;
+   const v1= req.body.fsinid;
+   const v2=req.body.fsinpasswd;
+   Tasksf.find({username :v1, password: v2},function(err,docs)
+   {
+       if(err)
+        res.send("err");
+       else
+       {
+        var x= console.log(Object.keys(docs).length);
+        x=Object.keys(docs).length;
+         if(x==0)
+          res.send("Login failed");
+         else
+          res.sendFile(str2 +"/views/dashboard.html");
+         // console.log(str2 +"/views/dashboard.html");
+       }
+   });
+   //console.log("data added successfully");
+  // res.send("Added to DB successfully");
+});
+
+app.post("/fsignup",function(req,res)
+{
+   const v1= req.body.fsupid;
+   const v2= req.body.fsupemail;
+   const v3=req.body.fsuppasswd;
+   const t1=new Tasksf({
+    emailid : v2,
+    username :v1,
+    password : v3
+  });
+   t1.save();
+   res.send("ID created succesfully");
+});
+
+
+app.post("/esignin.html",function(req,res)
+{
+  // const res2=req.body.s1;
+   const v1= req.body.fsinid;
+   const v2=req.body.fsinpasswd;
+   Taskse.find({username :v1, password: v2},function(err,docs)
+   {
+      //  if(err)
+      //   res.send("err");
+      //  else
+      //  {
+      //   var x= console.log(Object.keys(docs).length);
+      //   x=Object.keys(docs).length;
+      //    if(x==0)
+      //    //res.sendFile("..\views\logine.html");
+      //    else
+      //    //res.sendFile(str2 +"/views/dashboard.html");
+      //  }
+   });
+   //console.log("data added successfully");
+  // res.send("Added to DB successfully");
+});
+app.post("/esignup",function(req,res)
+{
+   const v1= req.body.fsupid;
+   const v2= req.body.fsupemail;
+   const v3=req.body.fsuppasswd;
+   const t1=new Taskse({
+    emailid : v2,
+    username :v1,
+    password : v3
+  });
+   t1.save();
+   res.send("ID created succesfully");
+});
+app.post("/gps.html",function(req,res)
+{
+    res.sendFile(str2+"/views/gps.html"); 
+});
+app.post("/psq.html",function(req,res)
+{
+    res.sendFile(str2+"/views/post.html"); 
+});
+app.post("/inp.html",function(req,res)
+{
+    const v1=req.body.str1;
+    console.log("v1 is " + v1);
+    const q1 =new QA({
+      ques:v1,
+      ans:"Empty :("
+    });
+    q1.save();
+    res.send("Ques added suucessfully");
+});
+
+
+
+app.get("/",function(req,res)
+{
+    const str=__dirname;
+    const str2= path.join(__dirname,"../");
+    res.sendFile(str2 + "/views/frontpage.html");
+   console.log(str2+ "/views/frontpage.html");
+   console.log("Running I guess");
+});
+app.get("/details",function(req,res)
+{
+  //res.send("Working rn");
+    Tasks.find(function(err,workings)
+    {
+         if(err)
+          console.log(err);
+        else
+        res.send(workings);
+    });
+});
+
+app.listen(3000,function()
+{
+    console.log("Server is running on port 3000");
+});
---
+++ frontpage/jsFiles/app1.js
@@ -0,0 +1,27 @@
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+
+const express=require("express");
+
+const port =  3000;
+
+const app = express();
+
+app.get('/',(err,res) => {
+  res.send("hh")
+});
+
+app.listen(port,function()
+{
+ console.log("Server is running");
+});
